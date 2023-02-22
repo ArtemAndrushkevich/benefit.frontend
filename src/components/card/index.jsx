0d816711222d760db99d9d3a7330b27d97f3b202
Artem <Andrushkevich.11@mail.ru>
2023-02-22 20:26:46 +0300

    fix

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 export default function Card({ title = '', img }) {
   return (
-    <div className="shop1">
+    <div className="shop">
       <span className="card-title">{title}</span>
       <img src={img} alt="картинка" className="card-img" />
     </div>
--- src/components/categories/index.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line react/prop-types, no-use-before-define
-export default function Categories({ title = '' }) {
-  return (
-    <div className="categories">
-      <span className="categories-title">{title}</span>
-
-    </div>
-  );
-}
